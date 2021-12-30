467ac2fd02b3b756286423ab40d22845e740e780
Praveen Singh <praveensingh2897@gmail.com>
2021-12-30 16:46:10 +0530

    ExpenseItem.js added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./ExpenseItem.css";
+
+const ExpenseItem = () => {
+  const DATE_OPTIONS = {
+    weekday: "short",
+    month: "short",
+    year: "numeric",
+    day: "numeric",
+  };
+
+  const date = new Date(2021, 2, 28).toLocaleDateString("en-US", DATE_OPTIONS);
+
+  const item = "Car Insurance";
+
+  const price = 300;
+
+  return (
+    <div className="expense-item">
+      <div>{date}</div>
+      <div className="expense-item__description">
+        <h2>{item}</h2>
+        <div className="expense-item__price">{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
