7f30329532cbd848ce17608a55959d8974b29d4b
Praveen Singh <praveensingh2897@gmail.com>
2022-03-16 03:25:38 +0530

    Expense form created

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import Card from "../UI/Card";
 
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const [Title, setTitle] = useState(title);
+
+  const changeTitle = () => {
+    setTitle("upadted");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{Title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
+      <button onClick={changeTitle}> Change Title</button>
     </Card>
   );
 };
